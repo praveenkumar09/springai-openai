f29b7aa26687436a3d5dd010fa592e3e952b4d2e
praveenkumar09 <tpkprav@gmail.com>
2025-11-03 19:16:31 +0800

    Initial commit

---
+++ src/main/java/com/praveen/springai_openai/SpringaiOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.springai_openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiOpenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/springai_openai/SpringaiOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.springai_openai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
