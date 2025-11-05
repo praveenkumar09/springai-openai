39199c72df68b3881d34068f0d10655669879d67
praveenkumar09 <tpkprav@gmail.com>
2025-11-05 19:34:03 +0800

    Added example for system and the user prompts

--- src/main/java/com/praveen/springai_openai/controller/ChatController.java
+++ src/main/java/com/praveen/springai_openai/controller/ChatController.java
@@ -20,7 +20,16 @@ public class ChatController {
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message) {
         return chatClient
-                .prompt(message)
+                .prompt()
+                .system("""
+                        You are an internal HR assistant. Your role is to\s
+                        help employees with their questions related to their\s
+                        HR Policies, such as leave policies, benefits and code\s
+                        of conduct. If a user asks for help with anything outside\s
+                        of these topics, kindly inform them you can only assist\s
+                        with queries related to HR policies.
+                        """)
+                .user(message)
                 .call()
                 .content();
     }
