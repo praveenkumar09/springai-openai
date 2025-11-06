baa73527e37d3d557d3c7595834d3d5a51433e25
praveenkumar09 <tpkprav@gmail.com>
2025-11-06 18:43:09 +0800

    Added example to respond as a list and map

--- src/main/java/com/praveen/springai_openai/controller/StructuredOutputController.java
+++ src/main/java/com/praveen/springai_openai/controller/StructuredOutputController.java
@@ -2,11 +2,17 @@ package com.praveen.springai_openai.controller;
 
 import com.praveen.springai_openai.record.CountryCities;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.ai.converter.StructuredOutputConverter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/structured")
@@ -27,6 +33,30 @@ public class StructuredOutputController {
                 .user(message)
                 .call()
                 .entity(CountryCities.class);
+        return ResponseEntity.ok(entity);
+    }
+
+    @GetMapping("chat-list")
+    public ResponseEntity<List<String>> chatList(
+            @RequestParam("message") String message
+    ) {
+        List<String> entity = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(new ListOutputConverter());
+        return ResponseEntity.ok(entity);
+    }
+
+    @GetMapping("chat-map")
+    public ResponseEntity<Map<String,Object>> chatMap(
+            @RequestParam("message") String message
+    ) {
+        Map<String,Object> entity = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(new MapOutputConverter());
         return ResponseEntity.ok(entity);
     }
 }
