9fe630373b4496d977e67215758c9984f00eb1e3
praveenkumar09 <tpkprav@gmail.com>
2025-11-06 18:48:18 +0800

    Added example to respond as parameterized POJO

--- src/main/java/com/praveen/springai_openai/controller/StructuredOutputController.java
+++ src/main/java/com/praveen/springai_openai/controller/StructuredOutputController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.ai.converter.StructuredOutputConverter;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +58,19 @@ public class StructuredOutputController {
                 .user(message)
                 .call()
                 .entity(new MapOutputConverter());
+        return ResponseEntity.ok(entity);
+    }
+
+    @GetMapping("chat-bean-list")
+    public ResponseEntity<List<CountryCities>> chatBeanList(
+            @RequestParam("message") String message
+    ) {
+        List<CountryCities> entity = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(new ParameterizedTypeReference<List<CountryCities>>() {
+                });
         return ResponseEntity.ok(entity);
     }
 }
