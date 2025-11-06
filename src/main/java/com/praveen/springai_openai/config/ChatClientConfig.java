7dd55dcff849d15c8b21af7ca269ab8e40926f30
praveenkumar09 <tpkprav@gmail.com>
2025-11-06 18:18:15 +0800

    Added stream example for chats

--- src/main/java/com/praveen/springai_openai/config/ChatClientConfig.java
+++ src/main/java/com/praveen/springai_openai/config/ChatClientConfig.java
@@ -4,14 +4,19 @@ import com.praveen.springai_openai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import java.util.List;
 import java.util.function.Supplier;
 
 @Configuration
 public class ChatClientConfig {
+
+    @Value("classpath:/promptTemplates/systemPromptTemplate.st")
+    Resource systemPromptTemplate;
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
@@ -21,14 +26,7 @@ public class ChatClientConfig {
                         new SimpleLoggerAdvisor(),
                         new TokenUsageAuditAdvisor()
                 ))
-                .defaultSystem("""
-                        You are an internal HR assistant. Your role is to\s
-                        help employees with their questions related to their\s
-                        HR Policies, such as leave policies, benefits and code\s
-                        of conduct. If a user asks for help with anything outside\s
-                        of these topics, kindly inform them you can only assist\s
-                        with queries related to HR policies.
-                        """)
+                .defaultSystem(systemPromptTemplate)
                 .build();
     }
 
