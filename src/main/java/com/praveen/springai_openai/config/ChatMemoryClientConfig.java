2b632377c83ac240ed73cc9cbbe97b0ebcb46f53
praveenkumar09 <tpkprav@gmail.com>
2025-11-08 18:37:03 +0800

    create own chatMemory Bean

--- src/main/java/com/praveen/springai_openai/config/ChatMemoryClientConfig.java
+++ src/main/java/com/praveen/springai_openai/config/ChatMemoryClientConfig.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,17 @@ import java.util.function.Supplier;
 
 @Configuration
 public class ChatMemoryClientConfig {
+
+    @Bean
+    public ChatMemory chatMemory(
+            JdbcChatMemoryRepository jdbcChatMemoryRepository
+    ){
+        return MessageWindowChatMemory
+                .builder()
+                .maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+    }
 
     @Bean("chatMemoryChatClient")
     public ChatClient chatMemoryChatClient(
