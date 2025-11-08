9abbf95af10cbc3bc56145bb5981db1f7b428547
praveenkumar09 <tpkprav@gmail.com>
2025-11-08 18:25:40 +0800

    Added sql properties to store the chat memory

--- src/main/java/com/praveen/springai_openai/controller/ChatMemoryController.java
+++ src/main/java/com/praveen/springai_openai/controller/ChatMemoryController.java
@@ -1,9 +1,12 @@
 package com.praveen.springai_openai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/memory")
@@ -17,11 +20,15 @@ public class ChatMemoryController {
 
     @RequestMapping("/chat")
     public String chat(
-            @RequestParam("message") String message
+            @RequestParam("message") String message,
+            @RequestHeader("username") String username
     ) {
         return chatMemoryChatClient
                 .prompt()
                 .user(message)
+                .advisors(
+                        advisorSpec ->
+                                advisorSpec.param(CONVERSATION_ID,username))
                 .call()
                 .content();
     }
