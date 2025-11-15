122fd39617aa2ef6ad67681aa20b77a0be914d7f
praveenkumar09 <tpkprav@gmail.com>
2025-11-15 10:31:47 +0800

    Added time tool to show time on asked user location dynamically with a Tool param

--- src/main/java/com/praveen/springai_openai/tools/TimeTools.java
+++ src/main/java/com/praveen/springai_openai/tools/TimeTools.java
@@ -3,8 +3,10 @@ package com.praveen.springai_openai.tools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Component;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.function.Supplier;
 
 @Component
@@ -19,5 +21,16 @@ public class TimeTools {
     public String getCurrentTime() {
         logger.info("Returning current time in users timezone");
         return LocalTime.now().toString();
+    }
+
+    @Tool(
+            name="getCurrentTime",
+            description = "Get the current time in the specified timezone"
+    )
+    public String getCurrentTime(
+            @ToolParam(description = "Value representing the time zone") String timeZone
+    ){
+        logger.info("Returning the current time in the specified timezone: {}",timeZone);
+        return LocalTime.now(ZoneId.of(timeZone)).toString();
     }
 }
