b86593815772f2ee718d618e8e89e9001eb98c83
praveenkumar09 <tpkprav@gmail.com>
2025-11-19 21:18:36 +0800

    Added jaeger tracing for more detailed analysis

---
+++ src/main/java/com/praveen/springai_openai/config/OpenTelemetryExporterConfig.java
@@ -0,0 +1,20 @@
+package com.praveen.springai_openai.config;
+
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenTelemetryExporterConfig {
+
+    @Bean
+    public OtlpGrpcSpanExporter otlpGrpcSpanExporter(
+            @Value("${spring.opentelemetry.exporter.otlp.endpoint}") String url
+    ) {
+        return OtlpGrpcSpanExporter
+                .builder()
+                .setEndpoint(url)
+                .build();
+    }
+}
