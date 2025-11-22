c15b7a2d0999db997c0ce591dd51656941523da2
praveenkumar09 <tpkprav@gmail.com>
2025-11-22 11:16:10 +0800

    Added text to speech with SpeechOptions

--- src/main/java/com/praveen/springai_openai/controller/SpeechController.java
+++ src/main/java/com/praveen/springai_openai/controller/SpeechController.java
@@ -1,6 +1,10 @@
 package com.praveen.springai_openai.controller;
 
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.ai.openai.audio.speech.SpeechModel;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +32,26 @@ public class SpeechController {
         byte[] audioInBytes = speechModel.call(message);
         Path path = Paths.get("out.mp3");
         Files.write(path, audioInBytes);
+        return "MP3 file " +
+                "saved successfully " +
+                "to " +path.toAbsolutePath();
+    }
+
+    @GetMapping("speech-options")
+    String speechOptions(
+            @RequestParam("message") String message
+    ) throws IOException {
+        SpeechResponse speechResponse = speechModel.call(
+                new SpeechPrompt(message,
+                        OpenAiAudioSpeechOptions.builder()
+                                .voice(OpenAiAudioApi
+                                        .SpeechRequest.Voice.NOVA)
+                                .speed(1.1f)
+                                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                                .build())
+        );
+        Path path = Paths.get("out.mp3");
+        Files.write(path, speechResponse.getResult().getOutput());
         return "MP3 file " +
                 "saved successfully " +
                 "to " +path.toAbsolutePath();
