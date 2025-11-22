9a55e5656216c38280d5d92bd731a5f8296e0127
praveenkumar09 <tpkprav@gmail.com>
2025-11-22 10:53:32 +0800

    Audio transcription prompt

--- src/main/java/com/praveen/springai_openai/controller/AudioController.java
+++ src/main/java/com/praveen/springai_openai/controller/AudioController.java
@@ -1,6 +1,10 @@
 package com.praveen.springai_openai.controller;
 
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +30,28 @@ public class AudioController {
     public String transcribe(){
         return openAiAudioTranscriptionModel
                 .call(audioFile);
+    }
+
+    @GetMapping("/transcribe-options")
+    public String transcribeWithOptions(){
+        AudioTranscriptionResponse audioTranscriptionResponse = openAiAudioTranscriptionModel
+                .call(
+                        new AudioTranscriptionPrompt(
+                                audioFile,
+                                OpenAiAudioTranscriptionOptions
+                                        .builder()
+                                        .prompt("Talking about Spring AI")
+                                        .language("en")
+                                        .temperature(0.5f)
+                                        .responseFormat(OpenAiAudioApi
+                                                .TranscriptResponseFormat
+                                                .VTT)
+                                        .build()
+                        )
+                );
+        return audioTranscriptionResponse
+                .getResult()
+                .getOutput();
     }
 
 
